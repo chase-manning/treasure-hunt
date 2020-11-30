1874dcdc200efcc6277725f7adda6e9ce785697b
Chase Manning <me@chasemanning.co.nz>
2020-11-30 17:14:04 +1300

    :art: Move App Location

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
 
 const StyledApp = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
-  background-color: red;
   height: 100%;
 `;
 
 function App() {
-  return <StyledApp>meow</StyledApp>;
+  return (
+    <StyledApp>
+      <GlobalStyles />
+    </StyledApp>
+  );
 }
 
 export default App;
