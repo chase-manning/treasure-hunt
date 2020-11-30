38eb06aa7966dc690c3ea9c2a95a2cb04ba90e6d
Chase Manning <me@chasemanning.co.nz>
2020-11-30 22:54:52 +1300

    :lipstick: Round the Percent Complete

--- src/Components/ProgressBar.tsx
+++ src/Components/ProgressBar.tsx
@@ -38,7 +38,7 @@ type Props = {
 };
 
 const ProgressBar = (props: Props) => {
-  const percentString = props.percentComplete * 100 + "%";
+  const percentString = Math.round(props.percentComplete * 100) + "%";
 
   return (
     <StyledProgressbar>
