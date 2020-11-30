e5dcd096653a9965e93ebb1c9bb1b0752f18eeb2
Chase Manning <me@chasemanning.co.nz>
2020-11-30 17:57:33 +1300

    :sparkles: Add Code Input to Continue

--- src/Components/Continue.tsx
+++ src/Components/Continue.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { StageType } from "../Stages/stages";
+import Button from "../styles/Button";
 
 const StyledContinue = styled.div`
   width: 100%;
@@ -10,27 +11,43 @@ const StyledContinue = styled.div`
   align-items: center;
 `;
 
-const Button = styled.button`
-  color: var(--light);
-  padding: 20px 40px;
-  background-color: var(--primary);
-  border-radius: 10px;
+const RiddleSection = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  border: solid 1px var(--sub);
+  border-radius: 5px;
+  margin-bottom: 20px;
 `;
 
 type Props = {
   stageType: StageType;
+  code?: string;
   nextPage: () => void;
 };
 
 const Continue = (props: Props) => {
   const [code, setCode] = useState("");
 
+  const checkCode = () => {
+    if (code === props.code) props.nextPage();
+  };
+
   return (
     <StyledContinue>
-      {props.stageType === StageType.CHALLENGE ? (
-        <Button onClick={() => props.nextPage()}>Complete</Button>
+      {props.stageType === StageType.RIDDLE ? (
+        <RiddleSection>
+          <Input></Input>
+          <Button onClick={() => props.nextPage()}>Continue</Button>
+        </RiddleSection>
       ) : (
-        <Button onClick={() => props.nextPage()}>Continue</Button>
+        <Button onClick={() => props.nextPage()}>Complete</Button>
       )}
     </StyledContinue>
   );
