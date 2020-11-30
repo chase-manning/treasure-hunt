12d489ba3742b4367a2718d0b155f50598665c07
Chase Manning <me@chasemanning.co.nz>
2020-11-30 21:20:22 +1300

    :sparkles: Add Sub Button Theme

--- src/styles/Button.tsx
+++ src/styles/Button.tsx
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
+type Props = {
+  sub?: boolean;
+};
+
 const Button = styled.button`
   margin-top: 20px;
-  color: var(--light);
+  color: ${(props: Props) => (props.sub ? "var(--primary)" : "var(--light)")};
   padding: 20px 40px;
-  background-color: var(--primary);
+  background-color: ${(props: Props) =>
+    props.sub ? "var(--light)" : "var(--primary)"};
+  border: solid 1px var(--primary);
   border-radius: 10px;
 `;
 
