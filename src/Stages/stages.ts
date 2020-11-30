7de48185487af7d857e8885784d0f1ecbca9619a
Chase Manning <me@chasemanning.co.nz>
2020-11-30 22:54:11 +1300

    :rocket: Create All Stages

--- src/Stages/stages.ts
+++ src/Stages/stages.ts
@@ -1,4 +1,5 @@
 import dude from "../assets/dude.png";
+import bikes from "../assets/bikes.png";
 
 export enum StageType {
   RIDDLE,
@@ -16,9 +17,9 @@ export type Stage = {
 };
 
 export type Riddle = {
+  location: string;
   clue: string;
   hint: string;
-  location: string;
   code: string;
 };
 
@@ -28,42 +29,207 @@ export type Challenge = {
 };
 
 export type Photo = {
+  location: string;
   image: string;
+  hint: string;
+  code: string;
 };
 
 export type Audio = {
+  location: string;
   sound: HTMLAudioElement;
+  hint: string;
+  code: string;
 };
 
 export const stages: Stage[] = [
   {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Hamster Wheel",
+      clue: "8,1,13,19,20,5,18,23,8,5,5,12",
+      hint: "1=A",
+      code: "2938",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Do a full 360 including going upside down",
+    },
+  },
+  {
     type: StageType.AUDIO,
     audio: {
+      location: "Tree Stump",
       sound: new Audio(
         "https://www.soundjay.com/mechanical/sounds/chainsaw-03.mp3"
       ),
-    },
-  },
-  {
-    type: StageType.PHOTO,
-    photo: {
-      image: dude,
-    },
-  },
-  {
-    type: StageType.RIDDLE,
-    riddle: {
-      clue: "A Catnap About Bulk",
-      hint: "Anagram",
-      location: "Takapuna Boat Club",
-      code: "1234",
+      hint: "Tree Stump",
+      code: "9149",
     },
   },
   {
     type: StageType.CHALLENGE,
     challenge: {
       group: true,
-      task: "Do 5 star jumps",
+      task: "Go down the biggest slide at the park",
     },
   },
+  {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Shower",
+      clue: "... .... --- .-- . .-.",
+      hint: "Morse",
+      code: "1034",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Have a shower",
+    },
+  },
+  //Comass thing
+  {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Takapuna Boat Club",
+      clue: "A Catnap About Bulk",
+      hint: "Anagram",
+      code: "1234",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task:
+        "Stand at the front of the biggest boat reinact the 'I'm Flying' Titanic Scene",
+    },
+  },
+  {
+    type: StageType.PHOTO,
+    photo: {
+      location: "Bike Sign",
+      image: bikes,
+      hint: "Franc's",
+      code: "1901",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Walk across the letters",
+    },
+  },
+  {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Giant Owl",
+      clue: "巨大なフクロウ",
+      hint: "Starbucks",
+      code: "1773",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Let the Owl give you a shoulder ride",
+    },
+  },
+  {
+    type: StageType.PHOTO,
+    photo: {
+      location: "Man Statue",
+      image: dude,
+      hint: "Servilles",
+      code: "9823",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Get a photo of a stranger posing with the statue",
+    },
+  },
+  //Liquor Store
+  {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Library",
+      clue: "Building with the most stories",
+      hint: "Stories has two meanings",
+      code: "8872",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Let everyone dress you, then get a library card",
+    },
+  },
+  // Library leading to tennis court
+  // Cannons
+  {
+    type: StageType.AUDIO,
+    audio: {
+      location: "The Elephant Wrestler",
+      sound: new Audio(
+        "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-audio-hero/audio_hero_ElephantsTrumpetAn_TE016001_344.mp3?_=2"
+      ),
+      hint:
+        "Ha ha ha It's an Elephant, bet you wrestled with that one for a while!",
+      code: "8112",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: true,
+      task:
+        "Buy the person taller than you a drink, tallest buys shortest, could be time for payback?",
+    },
+  },
+  //Cannons
+  {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Lake Pupuke",
+      clue: "-36.7853104, 174.7672319",
+      hint: "Jetty",
+      code: "3456",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Dunk your head underwater",
+    },
+  },
+  {
+    type: StageType.RIDDLE,
+    riddle: {
+      location: "Pools",
+      clue: "P+🏫-sch",
+      hint: "Swimming",
+      code: "1827",
+    },
+  },
+  {
+    type: StageType.CHALLENGE,
+    challenge: {
+      group: false,
+      task: "Go in and ask if you are allowed to swim naked",
+    },
+  },
+  //Treasure
 ];
