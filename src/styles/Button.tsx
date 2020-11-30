76f8e2d05b7cca40853fce18597ec81968f7fcd7
Chase Manning <me@chasemanning.co.nz>
2020-11-30 17:56:59 +1300

    :rocket: Create Button Component

---
+++ src/styles/Button.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: var(--light);
+  padding: 20px 40px;
+  background-color: var(--primary);
+  border-radius: 10px;
+`;
+
+export default Button;
