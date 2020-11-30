eb2bee792ff1b81bbd5fa9890a07e80368eb3610
Chase Manning <me@chasemanning.co.nz>
2020-11-30 17:14:33 +1300

    :sparkles: Create Global Styles Component

---
+++ src/Components/GlobalStyles.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --main: "black";
+        --sub: "grey";
+        --primary: "blue":
+    }
+  
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-family: "Roboto", sans-serif;
+    }
+  
+    button {
+      background: none;
+      border: none;
+      outline: none;
+    }
+  `;
+
+function GlobalStyles() {
+  return <GlobalStyle />;
+}
+
+export default GlobalStyles;
