f74cd535720943086ddee86fb830af061539813b
Chase Manning <me@chasemanning.co.nz>
2020-11-30 17:41:17 +1300

    :sparkles: Add Stage Types

--- src/Stages/stages.ts
+++ src/Stages/stages.ts
@@ -5,13 +5,33 @@ export enum StageType {
 
 export type Stage = {
   type: StageType;
+  riddle?: Riddle;
+  challenge?: Challenge;
+};
+
+export type Riddle = {
+  clue: string;
+  location: string;
+  code: string;
+};
+
+export type Challenge = {
+  task: string;
 };
 
 export const stages: Stage[] = [
   {
-    type: StageType.CHALLENGE,
+    type: StageType.RIDDLE,
+    riddle: {
+      clue: "Where the cat is not brown",
+      location: "In the Carpark",
+      code: "1234",
+    },
   },
   {
-    type: StageType.RIDDLE,
+    type: StageType.CHALLENGE,
+    challenge: {
+      task: "Do 5 star jumps",
+    },
   },
 ];
