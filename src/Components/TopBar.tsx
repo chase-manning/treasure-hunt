d71765de70055fa2fb1e80b0b5234deeb00aef08
Chase Manning <me@chasemanning.co.nz>
2020-12-02 02:32:33 +1300

    :sparkles: Create Top Bar Component

---
+++ src/Components/TopBar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { Stage, StageType } from "../Stages/stages";
+import ProgressBar from "./ProgressBar";
+import Hints from "./Hints";
+
+const StyledTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+type Props = {
+  stage: Stage;
+  active: boolean;
+  nextPage: () => void;
+  percentComplete: number;
+  useHint: () => void;
+  hints: number;
+};
+
+const TopBar = (props: Props) => {
+  if (!props.active) return null;
+
+  return (
+    <StyledTopBar>
+      <ProgressBar percentComplete={props.percentComplete} />
+      {props.stage.type === StageType.AUDIO && (
+        <Hints
+          hint={props.stage.audio!.hint}
+          useHint={() => props.useHint()}
+          hints={props.hints}
+        />
+      )}
+      {props.stage.type === StageType.RIDDLE && (
+        <Hints
+          hint={props.stage.riddle!.hint}
+          useHint={() => props.useHint()}
+          hints={props.hints}
+        />
+      )}
+      {props.stage.type === StageType.PHOTO && (
+        <Hints
+          hint={props.stage.photo!.hint}
+          useHint={() => props.useHint()}
+          hints={props.hints}
+        />
+      )}
+    </StyledTopBar>
+  );
+};
+
+export default TopBar;
